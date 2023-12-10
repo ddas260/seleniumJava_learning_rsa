435bb71554a565fa2874a6f7282cadcd226448d2
Dibyajyoti Das <dibyajyotidas.260@outlook.com>
2023-12-10 22:53:23 +0530

    Modified ActionsDemo.java, added code for keyDown, doubleClick, contextClick etc.

--- src/lessons/ActionsDemo.java
+++ src/lessons/ActionsDemo.java
@@ -3,6 +3,7 @@ package lessons;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -14,9 +15,13 @@ public class ActionsDemo {
 		//System.setProperty("webdriver.chrome.driver", null);
 		WebDriver chrome = new ChromeDriver();
 		chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		chrome.manage().window().maximize();
 		chrome.navigate().to("https://www.amazon.in/");
 		Actions a = new Actions(chrome); //creating an object of Action class
-		a.moveToElement(chrome.findElement(By.xpath("//span[@class='nav-line-2']/span[@class='nav-icon nav-arrow']"))).build().perform(); // mouse hover to "Account @ Lists"
+		//mouse hover to the search-box and click then press left-shift button and enter "macbook"(to make all in caps) then double click on the text to select all of the text
+		a.moveToElement(chrome.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.LEFT_SHIFT).sendKeys("macbook").doubleClick().build().perform();
+		// mouse hover to "Account @ Lists" and right-click on that
+		a.moveToElement(chrome.findElement(By.xpath("//a[@id='nav-link-accountList']"))).contextClick().build().perform();
 		
 
 	}
