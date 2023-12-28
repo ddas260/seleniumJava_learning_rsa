937e6ce6de29fa6e4c16c94ca693bedab145c380
Dibyajyoti Das <dibyajyotidas.260@outlook.com>
2023-12-28 17:02:36 +0530

    new class file create "Window Handles.Java"

---
+++ src/lessons/WindowHandles.java
@@ -0,0 +1,18 @@
+package lessons;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver chrome = new ChromeDriver();
+		chrome.navigate().to("http://rahulshettyacademy.com/loginpagePractise");
+		
+		
+
+	}
+
+}
+ 
