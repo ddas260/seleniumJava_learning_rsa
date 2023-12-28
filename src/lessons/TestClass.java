968e102a508b3f3dfeb8dcf211543292951402e3
Dibyajyoti Das <dibyajyotidas.260@outlook.com>
2023-12-28 20:56:59 +0530

    modified TestClass.java

--- src/lessons/TestClass.java
+++ src/lessons/TestClass.java
@@ -1,9 +1,14 @@
 package lessons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
 
 	}
 
