6e515a70834ec9011db3caa8f9a748b6e2bdf361
Dibyajyoti Das <dibyajyotidas.260@outlook.com>
2024-01-12 07:59:48 +0530

    Modified Calendar.java file

--- src/lessons/Calendar.java
+++ src/lessons/Calendar.java
@@ -3,16 +3,26 @@ package lessons;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver chrome = new ChromeDriver();
-		chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		//chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(chrome, Duration.ofSeconds(5));
 		chrome.get("https://www.path2usa.com/travel-companion/");
-		chrome.findElement(By.id("form-field-travel_comp_date")).click();
+		//chrome.manage().window().maximize();
+		
+		JavascriptExecutor js = (JavascriptExecutor)chrome;
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div/input[@id='form-field-travel_comp_date']")));
+		chrome.findElement(By.xpath("//div/input[@id='form-field-travel_comp_date']")).click();
 		
 		
 
