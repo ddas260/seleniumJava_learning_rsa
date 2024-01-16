682ea651d14d669c7f22c9e53abf2d0d1347920e
Dibyajyoti Das <dibyajyoti@tier5-00895>
2024-01-16 20:10:32 +0530

    Modified SSLCheck.java and YatraCalendar.java file

--- src/lessons/SSLCheck.java
+++ src/lessons/SSLCheck.java
@@ -7,12 +7,26 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class SSLCheck {
 
 	public static void main(String[] args) {
-		ChromeOptions options = new ChromeOptions();
-		options.setAcceptInsecureCerts(true);
-		WebDriver chrome = new ChromeDriver(options);
+		ChromeOptions options1 = new ChromeOptions();
+		options1.setAcceptInsecureCerts(true);
+		WebDriver chrome = new ChromeDriver(options1);
 		chrome.get("https://expired.badssl.com/");
 		System.out.println(chrome.getTitle());
+		//for Microsoft Edge
+//		EdgeOptions options2 = new EdgeOptions();
+//		options2.setAcceptInsecureCerts(true);
+//		WebDriver edge = new EdgeDriver(options2);
+//		edge.get("https://expired.badssl.com/");
+//		System.out.println(edge.getTitle());
+		//for Mozilla Firefox
+		//System.setProperty("webdriver.gecko.driver", "/home/dibyajyoti/BrowserDrivers/geckodriver-v0.34.0-linux64/geckodriver");
+//		FirefoxOptions options3 = new FirefoxOptions();
+//		options3.setAcceptInsecureCerts(true);
+//		WebDriver firefox = new FirefoxDriver(options3);
+//		firefox.get("https://expired.badssl.com/");
+//		System.out.println(firefox.getTitle());
 
 	}
+	
 
 }
--- src/lessons/YatraCalendar.java
+++ src/lessons/YatraCalendar.java
@@ -14,15 +14,26 @@ public class YatraCalendar {
 		chrome.findElement(By.cssSelector("#BE_flight_origin_date")).click();
 		Thread.sleep(2000);
 		List<WebElement> td = chrome.findElements(By.xpath("//tbody/tr/td"));
+		String date = null;
 		for (int i=0; i<td.size();i++) {
 			WebElement data = td.get(i);
 			String a = data.getAttribute("data-date");
 			if(a!=null) {
 				if(a.equalsIgnoreCase("18/02/2024")) {
+					date = a;
 					data.click();
 					break;
 				}
 			}
+			
 		}
+		//condition to check if the date is selected or not
+		if(date==null) {
+			System.out.println("No date found");
+		}
+		else {
+			System.out.println("Date is selected");
+		}
+		
 	}
 }
